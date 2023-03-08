b846bf3f45933281aeeafca351670144d85eb4e0
Abhinavcode13 <126642111+Abhinavcode13@users.noreply.github.com>
2023-03-08 22:40:51 +0530

    Create Test.r

---
+++ Intro-To-R lanuguage/Test.r
@@ -0,0 +1,25 @@
+vec1 <- c(1,2,23)
+vec2 <- c("a","b","c")
+vec3 <- c(TRUE,FALSE,FALSE)
+
+mixbag1 <-c(1,TRUE,2,FALSE)
+
+mixbag2 <-c(1,"a",2,"b")
+
+mixbag3 <-c(1,"a",TRUE,"b",FALSE)
+
+l1 <- list(1,"a",TRUE)
+
+l2 <- list(c(1,2,3),c("a","b","c"),c(TRUE,TRUE,FALSE))
+
+m1 <- matrix(c(1,2,3,4,5,6))
+
+vec1 <- c(1,2,3,4,5,6)
+
+vec2 <-c(7,8,9,,10,11,12)
+
+a1 <- array(c(vec1,vec2),dim=c(2,3,2))
+
+color <-factor(c("blue","yellow","red"))
+
+data.frame(fruit_name=c("Apple","banana","orange"),fruit_cost=c(10,20,30)) -> fruit
